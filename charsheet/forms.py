9900669118d2ad6ec9198879df6412e979d77832
Tony Morrissey <ajmorrissey@gmail.com>
2016-04-03 09:54:11 -0700

    Added some base index and views for teaching.

---
+++ charsheet/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from models import Character
+
+class CharacterForm(forms.ModelForm):
+    # TODO: Define other fields here
+
+    class Meta:
+        model = Character
+        exclude = ['id']
+
+    def __init__(self, *args, **kwargs):
+        super(CharacterForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(CharacterForm, self).clean()
+        return cleaned_data
