a9298b298e3e3bfd04dc0c97511ab851e9343dc2
William Pelzl <WilliamPelzl@Gmail.com>
2016-04-09 11:15:39 -0700

    added grids for alignment, added character fields

--- static/charsheet/js/style.js
+++ static/charsheet/js/style.js
@@ -10,12 +10,10 @@ $(document).ready(function(){
 });
 */
 
-$(document).on('show','.accordion', function (e) {
-     //$('.accordion-heading i').toggleClass(' ');
-     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
-});
-
-$(document).on('hide','.accordion', function (e) {
-    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
-    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+$(function (){
+    if($('#character').val()== "true"){
+         $("input:checkbox").prop('checked',true);
+    }else{
+        $("input:checkbox").prop('checked', false);
+    }
 });
